c0cb6487b605c8d39960a69a887cb15ced99b0cb
Caleb Hattingh <caleb.hattingh@gmail.com>
2022-09-06 21:54:58 +1000

    feat: add openmp-sys to support static linking to openmp

--- src/lib.rs
+++ src/lib.rs
@@ -80,6 +80,9 @@
 //!
 //! [ngt]: https://github.com/yahoojapan/NGT
 
+// See: https://gitlab.com/kornelski/openmp-rs#1-adding-rust-dependency
+extern crate openmp_sys;
+
 mod error;
 mod index;
 pub mod optim;
